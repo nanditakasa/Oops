7afbfc0dedc4e4caebbf823323c8329acc2aa8f7
Nandita <91262484+nanditakasa@users.noreply.github.com>
2022-11-14 11:31:22 +0530

    Create 6.4.cpp

---
+++ 6.4.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class code
+{
+	int id;
+	public:
+	code(){}
+	code (int a){ id = a;}
+	code(code &x)
+	{
+		id =x.id;
+	}
+	void display(void)
+	{
+		cout<<id;
+	}
+};
+int main()
+{
+	code A(100);
+	code B(A);
+	code C = A;
+	code D;
+	D = A;
+	cout<<"\n id of A:";A.display();
+	cout<<"\n id of B:";B.display();
+	cout<<"\n id of C:";C.display();
+	cout<<"\n id of D:";D.display();
+	return 0;
+}
+/*output:
+id of A:100
+ id of B:100
+ id of C:100
+ id of D:100*/
